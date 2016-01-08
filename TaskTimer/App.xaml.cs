72e4e18e6f2a592683c01bcdfb4c48682622eea3
xlfdll <xlfdll@gmail.com>
2016-01-09 08:32:11 +0900

    Version 1.2: Updated application manifest file, as well as code cleanup

--- TaskTimer/App.xaml.cs
+++ TaskTimer/App.xaml.cs
@@ -5,7 +5,5 @@ namespace TaskTimer
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class App : Application
-    {
-    }
+    public partial class App : Application { }
 }
